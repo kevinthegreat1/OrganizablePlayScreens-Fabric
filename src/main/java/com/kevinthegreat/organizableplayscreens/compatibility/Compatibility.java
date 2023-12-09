7ea865bf9e7e0ed0d82bc98c560e46b5d77807e4
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-12-09 00:47:33 -0500

    Uncomment essential compatibility

--- src/main/java/com/kevinthegreat/organizableplayscreens/compatibility/Compatibility.java
+++ src/main/java/com/kevinthegreat/organizableplayscreens/compatibility/Compatibility.java
@@ -1,10 +1,10 @@
 package com.kevinthegreat.organizableplayscreens.compatibility;
 
-//import gg.essential.api.EssentialAPI;
+import gg.essential.api.EssentialAPI;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class Compatibility {
     public static boolean essential_preventMultiplayerFeatures() {
-        return FabricLoader.getInstance().isModLoaded("essential")/* && EssentialAPI.getConfig().getCurrentMultiplayerTab() != 0*/;
+        return FabricLoader.getInstance().isModLoaded("essential") && EssentialAPI.getConfig().getCurrentMultiplayerTab() != 0;
     }
 }
