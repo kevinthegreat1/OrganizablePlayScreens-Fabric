e123de2301b876b5a383c59dbe45bf59298dac1c
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2025-10-27 17:22:08 -0400

    Add folder operations to client game test

--- src/gametest/java/com/kevinthegreat/organizableplayscreens/OrganizablePlayScreensClientGameTest.java
+++ src/gametest/java/com/kevinthegreat/organizableplayscreens/OrganizablePlayScreensClientGameTest.java
@@ -1,11 +1,19 @@
 package com.kevinthegreat.organizableplayscreens;
 
+import com.kevinthegreat.organizableplayscreens.gui.AbstractFolderEntry;
+import com.kevinthegreat.organizableplayscreens.gui.MultiplayerFolderEntry;
+import com.kevinthegreat.organizableplayscreens.gui.SingleplayerFolderEntry;
+import com.kevinthegreat.organizableplayscreens.mixin.accessor.EntryListWidgetInvoker;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
 import net.fabricmc.fabric.api.client.gametest.v1.context.TestSingleplayerContext;
 import net.fabricmc.fabric.api.client.gametest.v1.screenshot.TestScreenshotComparisonOptions;
 import net.fabricmc.fabric.mixin.client.gametest.ScreenAccessor;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
+import net.minecraft.client.gui.screen.world.WorldListWidget;
+import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.util.Nullables;
 
@@ -16,44 +24,71 @@ import java.util.Optional;
 public class OrganizablePlayScreensClientGameTest implements FabricClientGameTest {
     @Override
     public void runTest(ClientGameTestContext context) {
-        // Create a new world
+        // Create a new world and navigate to the select world screen
         //noinspection EmptyTryBlock, unused
         try (TestSingleplayerContext singleplayer = context.worldBuilder().create()) {}
         context.waitForScreen(TitleScreen.class);
-        // Navigate to the select world screen
         context.clickScreenButton("menu.singleplayer");
-        // Create a new folder
+        // Create a new folder and assert the select world screen root
         createNewFolder(context);
-        // Assert the select world screen
-        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("select-world-screen").save());
+        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("select-world-screen-root").save());
+
+        // Move the world into the folder
+        clickListWidgetEntry(context, WorldListWidget.WorldEntry.class);
+        clickFolderMoveInto(context, SingleplayerFolderEntry.class);
+        // Open the folder
+        clickListWidgetEntry(context, SingleplayerFolderEntry.class);
+        clickListWidgetEntry(context, SingleplayerFolderEntry.class);
+        // Create a new folder, section, and separator inside the folder and assert the select world screen folder
+        createNewFolder(context);
+        createNewEntry(context, "organizableplayscreens:entry.section");
+        createNewEntry(context, "organizableplayscreens:entry.separator");
+        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("select-world-screen-folder").save());
 
         // Navigate back to the title screen
         clickScreenButton(context, "←");
+        clickScreenButton(context, "←");
         context.waitForScreen(TitleScreen.class);
 
-        // Navigate to the multiplayer screen
+        // Navigate to the multiplayer screen, skip the multiplayer warning screen, and add a new server
         context.clickScreenButton("menu.multiplayer");
-        // Skip the multiplayer warning screen
         context.clickScreenButton("gui.proceed");
-        // Add a new server
         context.clickScreenButton("selectServer.add");
         context.clickScreenButton("addServer.add");
-        // Create a new folder
+        // Create a new folder and assert the multiplayer screen root
         createNewFolder(context);
-        // Assert the multiplayer screen
-        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("multiplayer-screen").save());
+        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("multiplayer-screen-root").save());
+
+        // Move the server into the folder
+        clickListWidgetEntry(context, MultiplayerServerListWidget.ServerEntry.class);
+        clickFolderMoveInto(context, MultiplayerFolderEntry.class);
+        // Open the folder
+        clickListWidgetEntry(context, MultiplayerFolderEntry.class);
+        clickListWidgetEntry(context, MultiplayerFolderEntry.class);
+        // Create a new folder, section, and separator inside the folder and assert the multiplayer screen folder
+        createNewFolder(context);
+        createNewEntry(context, "organizableplayscreens:entry.section");
+        createNewEntry(context, "organizableplayscreens:entry.separator");
+        context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("multiplayer-screen-folder").save());
 
         // Navigate back to the title screen
+        clickScreenButton(context, "←");
         clickScreenButton(context, "←");
         context.waitForScreen(TitleScreen.class);
     }
 
     private void createNewFolder(ClientGameTestContext context) {
-        // Create a new folder
+        createNewEntry(context, "organizableplayscreens:folder.folder");
+    }
+
+    private void createNewEntry(ClientGameTestContext context, String translationKey) {
+        // Click the add entry button
         clickScreenButton(context, "+");
-        // Assert the new folder screen
+        // Assert the new entry screen
         context.assertScreenshotEquals(TestScreenshotComparisonOptions.of("new-folder-screen").save());
-        // Finish creating the new folder
+        // Click the entry type button
+        context.clickScreenButton(translationKey);
+        // Finish creating the new entry
         context.clickScreenButton("gui.done");
     }
 
@@ -71,5 +106,45 @@ public class OrganizablePlayScreensClientGameTest implements FabricClientGameTes
                     throw new AssertionError("Could not find button '%s' in screen '%s'".formatted(text, Nullables.map(client.currentScreen, screen -> screen.getClass().getName())));
                 })
         );
+    }
+
+    private <T extends AlwaysSelectedEntryListWidget.Entry<? super T>> void clickListWidgetEntry(ClientGameTestContext context, Class<T> entryClass) {
+        context.runOnClient(client -> {
+            AlwaysSelectedEntryListWidget<?> listWidget = getListWidget(client);
+            T entry = getListWidgetEntry(listWidget, entryClass);
+            int i = listWidget.children().indexOf(this);
+            int x = listWidget.getRowLeft();
+            int y = ((EntryListWidgetInvoker) listWidget).rowTop(i);
+            int w = listWidget.getRowWidth();
+            entry.mouseClicked(x + w, y, 0);
+        });
+    }
+
+    private <T extends AlwaysSelectedEntryListWidget.Entry<? super T> & AbstractFolderEntry<?, ? super T>> void clickFolderMoveInto(ClientGameTestContext context, Class<T> folderClass) {
+        context.runOnClient(client -> {
+            AlwaysSelectedEntryListWidget<?> listWidget = getListWidget(client);
+            T folderEntry = getListWidgetEntry(listWidget, folderClass);
+            folderEntry.getButtonMoveInto().onClick(folderEntry.getButtonMoveInto().getX(), folderEntry.getButtonMoveInto().getY());
+        });
+    }
+
+    private AlwaysSelectedEntryListWidget<?> getListWidget(MinecraftClient client) {
+        return Optional.ofNullable(client.currentScreen)
+                .map(ScreenAccessor.class::cast)
+                .map(ScreenAccessor::getDrawables)
+                .orElse(List.of())
+                .stream()
+                .filter(AlwaysSelectedEntryListWidget.class::isInstance)
+                .map(AlwaysSelectedEntryListWidget.class::cast)
+                .findAny()
+                .orElseThrow(() -> new AssertionError("Could not find list widget in screen '%s'".formatted((Object) Nullables.map(client.currentScreen, screen -> screen.getClass().getName()))));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends AlwaysSelectedEntryListWidget.Entry<? super T>> T getListWidgetEntry(AlwaysSelectedEntryListWidget<?> listWidget, Class<T> entryClass) {
+        return (T) listWidget.children().stream()
+                .filter(entryClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new AssertionError("Could not find entry of type '%s' in list widget '%s'".formatted(entryClass.getName(), listWidget.getClass().getName())));
     }
 }
